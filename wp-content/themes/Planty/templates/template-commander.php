f518f62cb9e4e8e9f318a986a027924bc5289ed4
Adrien Unzel <adrien.unzel@neuf.fr>
2023-11-14 16:56:22 +0100

    Mise en page

---
+++ wp-content/themes/Planty/templates/template-commander.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: template commander
+ */
+
+get_header();
+?>
+
+<main id="site-content">
+
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php
+get_footer();
